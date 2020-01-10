d76a8bf62faef8ba053b9e7736f1d3435d88f489
GuiLec <glecallier@yahoo.com>
2020-01-10 22:19:56 +0100

    Change title and ID

--- src/modules/dictations/dictations.service.ts
+++ src/modules/dictations/dictations.service.ts
@@ -5,8 +5,8 @@ import { Dictation } from './dictations.interface';
 export class DictationsService {
   private readonly dictations: Dictation[] = [
     {
-      id: '1',
-      name: 'Les courses en mer',
+      id: '22',
+      name: 'Dictée from API',
       text:
         'Les courses en mer\n\nSur les quais, la foule agglutinée qui lance à cor et à cri un au revoir aux équipages annonce l\'imminence du départ. "Ohé !" Des mains et des mouchoirs, semblables à des oriflammes bariolées, sont agités par la famille, les amis, des enfants... Puis les ancres surjalées sont relevées: "Larguez les amarres !"\n\nQu\'il s\'agisse de Christophe Colomb, de Florence Arthaud ou du charismatique Eric Tabarly, que ce soit à bord de trois-mâts, de catamarans ou de simples canots, les navigateurs, inlassables, depuis la nuit des temps, ont sillonné les océans. Tous ces découvreurs d\'îlots inconnus, ces marins sportifs familiers des top niveaux et ces scientifiques de haut vol ont confié leur phénoménal destin à la mer.',
       choiceInputs: [
